487adc4b319320e38f573523171fb76cd8fb50e1
HolyKrabs <dtkhai.game@gmail.com>
2025-02-01 19:05:20 +0700

    Initial commit

---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal class RoomGenerator
+{
+    private int roomSize;
+    public RoomGenerator(int roomSize)
+    {
+        this.roomSize = roomSize;
+    }
+
+    public Vector2Int RoomGeneration(HashSet<Vector2Int> Pos)
+    {
+        int walkLength = roomSize * 2;
+
+    }
+}
